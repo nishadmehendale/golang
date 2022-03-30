3620fd64b9087525deb2e892cb45075f20041908
Nishad Mehendale <26198718+nishadmehendale@users.noreply.github.com>
2022-03-30 19:48:54 +0530

    Idioswitch example

--- conditionals/conditionals.go
+++ conditionals/conditionals.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -36,4 +38,19 @@ func main() {
 		fmt.Println("Match Not Found")
 	}
 
+	// Idioswitch Example
+	switch tmpNum := random(); tmpNum {
+	case 1, 3, 5, 7, 9:
+		fmt.Println("We got an odd number", tmpNum)
+	case 2, 4, 6, 8, 0:
+		fmt.Println("We got an even number", tmpNum)
+	default:
+		fmt.Println("We did not get a number", tmpNum)
+	}
+
+}
+
+func random() int {
+	rand.Seed(time.Now().Unix())
+	return rand.Intn(10)
 }
