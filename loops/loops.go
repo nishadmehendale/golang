2ec5d04b2fc5cbbf79e61be7e617636042351c4d
Nishad Mehendale <26198718+nishadmehendale@users.noreply.github.com>
2022-04-17 21:19:38 +0530

    For loop over a range and nested loops

--- loops/loops.go
+++ loops/loops.go
@@ -20,4 +20,27 @@ func main() {
 		fmt.Println(timer)
 		time.Sleep(1 * time.Second)
 	}
+
+	/*
+		Looping over a collection / range
+	*/
+	courses := []string{"Course One", "Course Two"}
+	// Prints index value
+	for i := range courses {
+		fmt.Println(i)
+	}
+	// Prints course name
+	for _, i := range courses {
+		fmt.Println(i)
+	}
+	coursesCompleted := []string{"Course One"}
+	// Nested Loops
+	for _, i := range courses {
+		for _, j := range coursesCompleted {
+			if i == j {
+				break
+			}
+			fmt.Println("Courses In Progress", i)
+		}
+	}
 }
